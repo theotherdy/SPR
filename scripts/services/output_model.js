5f15481ba737c71e1d704e0946d411eb4b42e9d5
Jon Mason <jon.mason@medsci.ox.ac.uk>
2015-12-02 08:18:39 +0000

    Fixed problem with pushing values into intermediateTimeOn
    Created plotSingleCoordinate function, called for the first time from
    plotCoordinates. plotSingleCoordinate pushes the values onto the array
    and, if more steps need to be taken, calls itself again after a timeout.
    
    Using angular's $timeout for timeouts

--- scripts/services/output_model.js
+++ scripts/services/output_model.js
@@ -2,74 +2,80 @@
 
 /* 1. registering modules, services and constants */
 angular.module('output_model', ['system_model', 'experiment_status', 'cookies'])
-	.service('outputModel', ['systemModel', 'experimentStatus', '$cookies', outputMethod]);
+	.service('outputModel', ['systemModel', 'experimentStatus', '$cookies', '$timeout', outputMethod]);
 
-function outputMethod(systemModel, experimentStatus, $cookies) { 
+function outputMethod(systemModel, experimentStatus, $cookies, $timeout) { 
 
 /* 2. creating sub-methods as part of the function object that can be called */
 
 /* a) defining arrays to store data locally */
-	this.fLC = []; // fLC  must be <= 0, to obtain background value
-	this.timeOn = []; // timeOn must be < 0
-	this.timeOff = []; // timeOff must be < 0
-	this.RU_OnAdjusted = [];
 	var call = this; // create a specific selector for intermediate points function
+	call.fLC = []; // fLC  must be <= 0, to obtain background value
+	call.timeOn = []; // timeOn must be < 0
+	call.timeOff = []; // timeOff must be < 0
+	call.RU_OnAdjusted = [];
 	call.intermediateTimeOn = [];
 
 /* b) check for cookies and restore or create new */
 
 
 /* c) set fLC: user input via form; variable */
-	this.add_fLC = function(new_fLC) {
-		this.fLC.push(new_fLC);
+	call.add_fLC = function(new_fLC) {
+		call.fLC.push(new_fLC);
 	};
 
 /* d) set timeOn: user input via form; variable */
-	this.add_timeOn = function(new_timeOn) {
-		this.timeOn.push(new_timeOn);
+	call.add_timeOn = function(new_timeOn) {
+		call.timeOn.push(new_timeOn);
 	};		
 
 /* e) set timeOff: user input via form; variable */
-	this.add_timeOff = function(new_timeOff) {
-		this.timeOff.push(new_timeOff);
+	call.add_timeOff = function(new_timeOff) {
+		call.timeOff.push(new_timeOff);
 	};
 
 /* f) find the RU increase when R is totally saturated by L; constant */
-	this.find_RU_Max = function(sys_tRC, sys_mwR, con_vol, con_RPUM, sys_mwL, sys_mwLR) {
-		this.RU0 = sys_tRC*sys_mwR*con_vol*con_RPUM;
-		this.RU_MaxL = sys_tRC*sys_mwL*con_vol*con_RPUM;
-		this.RU_MaxLR = sys_tRC*sys_mwLR*con_vol*con_RPUM;
+	call.find_RU_Max = function(sys_tRC, sys_mwR, con_vol, con_RPUM, sys_mwL, sys_mwLR) {
+		call.RU0 = sys_tRC*sys_mwR*con_vol*con_RPUM;
+		call.RU_MaxL = sys_tRC*sys_mwL*con_vol*con_RPUM;
+		call.RU_MaxLR = sys_tRC*sys_mwLR*con_vol*con_RPUM;
 	};
 
 /* g) find RU_On: derived from 2nd order association formula; variable */
-	this.calc_RU_On = function(out_RU_MaxL, out_fLC, sys_Kd, sys_kOn, sys_kOff, out_timeOn, out_RU0, RU0_set) {
-		this.RU_On = ((out_RU_MaxL*out_fLC)/(sys_Kd+out_fLC))*(1-Math.pow(Math.E,-(sys_kOn*out_fLC+sys_kOff)*out_timeOn));
-		this.RU_OnAdjusted.push(this.RU_On+out_RU0-RU0_set);
+	call.calc_RU_On = function(out_RU_MaxL, out_fLC, sys_Kd, sys_kOn, sys_kOff, out_timeOn, out_RU0, RU0_set) {
+		call.RU_On = ((out_RU_MaxL*out_fLC)/(sys_Kd+out_fLC))*(1-Math.pow(Math.E,-(sys_kOn*out_fLC+sys_kOff)*out_timeOn));
+		call.RU_OnAdjusted.push(call.RU_On+out_RU0-RU0_set);
 	};
 
 /* h) find RU_Off: derived from 1st order disassociation formula; variable */
-	this.calc_RU_Off = function(out_RU_On, sys_kOff, out_timeOff, out_RU0, out_RU0_set) {
-		this.RU_Off = out_RU_On*(Math.pow(Math.E, -sys_kOff*out_timeOff));
-		this.RU_OffAdjusted = this.RU_Off+out_RU0-out_RU0_set;
+	call.calc_RU_Off = function(out_RU_On, sys_kOff, out_timeOff, out_RU0, out_RU0_set) {
+		call.RU_Off = out_RU_On*(Math.pow(Math.E, -sys_kOff*out_timeOff));
+		call.RU_OffAdjusted = call.RU_Off+out_RU0-out_RU0_set;
 	};
 
 /* j) generate intermediate outputs for SPR graph of association and disassocation to plot */
-	this.plotCoordinates = function(out_timeOn) {
-		var steps = 4;
-			// intermediate points for time on (x-axis coordinate 1)
-		var valTimeOn; // define variable to push
+	call.plotCoordinates = function(out_timeOn) {
 		call.intermediateTimeOn.length = 0; // clear previous graph points
+		var steps = 4;
+		var step = 0;
+		
+		// intermediate points for time on (x-axis coordinate 1)
+		call.plotSingleCoordinate(out_timeOn, step, steps); 
+
+		//var steps = 4;
+		//var valTimeOn; // define variable to push
+		//call.intermediateTimeOn.length = 0;
 		/* call.intermediateTimeOn.push(1); */ // test to check if push is working, which works
-		var createTimeOnIntermediate = function(i) {
-			setTimeout(function(){
-				valTimeOn = i*(out_timeOn/steps);
-				call.intermediateTimeOn.push(valTimeOn); // value is correct and function is working but its not pushing into array
+		//var createTimeOnIntermediate = function(i) {
+		//	setTimeout(function(){
+		//		valTimeOn = i*(out_timeOn/steps);
+		//		call.intermediateTimeOn.push(valTimeOn); // value is correct and function is working but its not pushing into array
 				/* alert(valTimeOn); */ // test to see if setTimeout is working, which does
-			}, 1500);
-		};
-		for (i = 0; i <= steps; i++) {
-			createTimeOnIntermediate(i);
-		}
+		//	}, 1500);
+		//};
+		///for (i = 0; i <= steps; i++) {
+		//	createTimeOnIntermediate(i);
+		//}
 
 		// intermediate points for RU On (y-axis coordinate 1)
 
@@ -77,4 +83,13 @@ function outputMethod(systemModel, experimentStatus, $cookies) {
 
 		// intemediate points for RU Off (y-axis coordinate 2)
 	};
+	
+	call.plotSingleCoordinate = function(out_timeOn, step, steps) {
+		call.intermediateTimeOn.push(step*(out_timeOn/steps))
+
+		if(step < steps) {
+			step++;
+			$timeout(function() {call.plotSingleCoordinate(out_timeOn, step, steps)}, 1500);
+		}
+	}
 }
