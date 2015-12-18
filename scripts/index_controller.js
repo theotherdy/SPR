9ecfab17ce097190a37573a2835ca29779adf3aa
csrisakvarakul <c.srisakvarakul@gmail.com>
2015-12-18 11:18:07 +0000

    built cookies function but faced 2 problem. (1) cookies not loading (2) cookies size stored breached the limit

--- scripts/index_controller.js
+++ scripts/index_controller.js
@@ -20,36 +20,75 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 	view.cookies = $cookies;
 	view.vol = vol;
 	view.RPUM = RPUM;
+		// default function of various buttons
 	view.backgroundSet = 0;
 	view.isDisabled_background = false;
 	view.isDisabled_run = false;
 	view.isDisabled_wash = true;
+		// stored cookies data
+	view.storedDataPrompt = false;
+	view.cookiesData = {
+			// stored controller state
+		stored_backgroundSet: view.backgroundSet,
+		stored_isDisabled_background: view.isDisabled_background,
+		stored_isDisabled_run: view.isDisabled_run,
+		stored_isDisabled_wash: view.isDisabled_wash,
+			// stored system
+		stored_tRC: view.system.tRC,
+		storedKd: view.system.Kd,
+		stored_kOff: view.system.kOff,
+		stored_mwL: view.system.mwL,
+		stored_mwR: view.system.mwR,
+			// stored experiment status
+		storedSteps: view.experiment.steps,
+		storedTimeOfDay: view.experiment.timeOfDay,
+			// stored output
+		stored_fLC_tableDisplay: view.output.fLC_tableDisplay,
+		stored_fLC: view.output.fLC,
+		store_timeOn: view.output.timeOn,
+		storeRU_On_Output: view.output.RU_On_Output,
+		storeRU_CompiledLabelPlotAll: view.output.RU_CompiledLabelPlotAll,
+		store_tableData: view.table.data
 
-/* b) creating function for switching fLC input to mM magnitude */
+	};
+
+/* b) creating functions for the cookies prompt */
+	view.continueSaved = function() {
+		view.storedDataPrompt = false; // close the prompt
+	};
+
+	view.restartExperiment = function(){
+		view.cookies.remove("storedData");
+		view.system.loadNewPair(view.vol, view.RPUM);
+		view.restart();
+		view.storedDataPrompt = false;
+	};
+
+/* c) creating function for switching fLC input to mM magnitude */
 	view.magnitude_mM = function() {
 		view.output.magnitudeAdjust = 1000;
 		view.output.unitAdjust = "mM";
 	};
 
-/* c) creating function for switching fLC input to uM magnitude */
+/* d) creating function for switching fLC input to uM magnitude */
 	view.magnitude_uM = function() {
 		view.output.magnitudeAdjust = 1000000;
 		view.output.unitAdjust = "uM";
 	};
 
-/* d) creating function for switching fLC input to nM magnitude */
+/* e) creating function for switching fLC input to nM magnitude */
 	view.magnitude_nM = function() {
 		view.output.magnitudeAdjust = 1000000000;
 		view.output.unitAdjust = "nM";
 	};
 
-/* e) creating function for "setup" button */
+/* f) creating function for "setup" button */
 	view.setup = function () {
 		view.experiment.timeOfDayCounter();
 		view.experiment.dayOfExperimentCounter();
 	};
 
-/* f) creating function for set "zero" button */
+/* g) creating function for set "zero" button */
 	view.set_background = function() {
 		view.backgroundSet = view.output.RU_On_Output[view.output.RU_On_Output.length-1];
 		view.isDisabled_background = true;
@@ -58,7 +97,7 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		}
 	};
 
-/* g) creating function for "run experiment" button  */
+/* h) creating function for "run experiment" button  */
 	view.runExperiment = function (new_fLC, new_timeOn) {
 		view.output.add_fLC(new_fLC);
 		view.output.add_timeOn(new_timeOn);
@@ -69,33 +108,35 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.experiment.timeOfDayCounter();
 		view.isDisabled_run = true;
 		view.isDisabled_wash = false;
+		view.cookies.putObject("storedData", view.cookiesData);
 	};
 
-/* h) creating function for "wash-up" button */
+/* i) creating function for "wash-up" button */
 	view.washUp = function(new_timeOn) {
 		view.output.plotCoordinatesOff(view.output.currentStep, view.output.totalSteps, new_timeOn, view.system.kOff, view.system.RU0, view.backgroundSet);
 		view.output.plotCompileLabelOff();
 		view.table.compileData(angular.copy(view.experiment.steps), view.output.fLC_tableDisplay[view.output.fLC_tableDisplay.length-1]*view.output.magnitudeAdjust, view.output.timeOn[view.output.timeOn.length-1], view.output.RU_On_Output[view.output.RU_On_Output.length-1]);
 		view.isDisabled_run = false;
 		view.isDisabled_wash = true;
+		view.cookies.putObject("storedData", view.cookiesData);
 	}; 
 
-/* i) creating function for "eat" button */
+/* j) creating function for "eat" button */
 	view.eat = function () {
 		view.experiment.timeOfDayCounter();
 	};
 
-/* j) creating function for "home" button */
+/* k) creating function for "home" button */
 	view.goHome = function () {
 		view.experiment.timeOfDay = view.experiment.startOfDay;
 	};
 
-/* k) creating function for "clear graph" button */
+/* l) creating function for "clear graph" button */
 	view.clearChart = function() {
 		view.output.RU_CompiledLabelPlotAll.length = 0;
 	};
 
-/* l) creating a function for "restart" button */
+/* m) creating a function for "restart" button */
 	view.restart = function () {
 		view.experiment.daysLeft = view.experiment.daysAllowed;
 		view.experiment.timeOfDay = view.experiment.startOfDay;
@@ -111,24 +152,38 @@ function viewMethod(systemModel, outputModel, experimentStatus, chartConfig, tab
 		view.table.data.length = 0;
 	};
 
-/* m) initialise application to generate unique values for the new system */
-	if (view.cookies.get("storedData") === null) {
+/* n) initialise application to generate unique values for the new system, else load previous experiment state from cookies */
+
+	if (view.cookies.getObject("storedData") === undefined) { // if there are no data, generate a new system
 		view.system.loadNewPair(view.vol, view.RPUM);
 		view.storedDataPrompt = false;
-	} else {
+		view.cookies.putObject("storedData", view.cookiesData);
+	} else { // load stored data
 		view.cookies.getObject("storedData");
+			// set controller state to as stored
+		view.backgroundSet = view.cookiesData.stored_backgroundSet;
+		view.isDisabled_background = view.cookiesData.stored_isDisabled_background;
+		view.isDisabled_run = view.cookiesData.stored_isDisabled_run;
+		view.isDisabled_wash = view.cookiesData.stored_isDisabled_wash;
+			// set system to as stored
+		view.system.tRC = view.cookiesData.stored_tRC;
+		view.system.Kd = view.cookiesData.storedKd;
+		view.system.kOff = view.cookiesData.stored_kOff;
+		view.system.find_kOn(view.system.Kd, view.system.kOff);
+		view.system.mwL = view.cookiesData.stored_mwL;
+		view.system.mwR = view.cookiesData.stored_mwR;
+		view.system.find_mwLR(view.system.mwL, view.system.mwR);
+		view.system.find_RU_Max(view.system.tRC, view.system.mwR, view.vol, view.RPUM, view.system.mwL, view.system.mwLR);
+			// set experiment status to as stored
+		view.experiment.steps = view.cookiesData.storedSteps;
+		view.experiment.timeOfDay = view.cookiesData.storedTimeOfDay;
+			// set output to as stored
+		view.output.fLC_tableDisplay = view.cookiesData.stored_fLC_tableDisplay;
+		view.output.fLC = view.cookiesData.stored_fLC;
+		view.output.timeOn = view.cookiesData.store_timeOn;
+		view.output.RU_On_Output = view.cookiesData.storeRU_On_Output;
+		view.output.RU_CompiledLabelPlotAll = view.cookiesData.storeRU_CompiledLabelPlotAll;
+		view.table.data = view.cookiesData.store_tableData;
 		view.storedDataPrompt = true;
 	}
-
-/* n) creating functions for the prompt */
-	view.continueSaved = function() {
-		view.storedDataPrompt = false; // close the prompt
-	};
-
-	view.restartExperiment = function(){
-		view.cookies.remove("storedData");
-		view.system.loadNewPair(view.vol, view.RPUM);
-		view.restart();
-		view.storedDataPrompt = false;
-	};
 }
